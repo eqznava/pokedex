34155a1dd161e5eec47713b05de1ab947950a093
Sergio Enriquez <eqz.nava@gmail.com>
2019-08-04 17:57:53 -0500

    Add javascript

---
+++ js/interaction.js
@@ -0,0 +1,19 @@
+const $openClosePokedex = document.getElementById('open-poke');
+const $caseLefTo = document.getElementById('caseLeft');
+const $openDoor = document.getElementById('door');
+
+$openClosePokedex.addEventListener('click', function(){
+  if ($caseLefTo.classList.contains('leftTo')) {
+    $caseLefTo.classList.remove('leftTo');
+    $openDoor.classList.remove('open-door-visible');
+  } else {
+    $caseLefTo.classList.add('leftTo');
+    $openDoor.classList.add('open-door-visible');
+  }
+});
+
+// $openClosePokedex.addEventListener('click', function(){
+//   if($openDoor.classList.contains('open-door')){
+//   }else {
+//   }
+// });
