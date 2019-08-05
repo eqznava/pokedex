cd31a9583e310eb1815ef8fbd83f762b4f9b2080
Sergio Enriquez <eqz.nava@gmail.com>
2019-08-04 21:17:34 -0500

    performance of the open and close action

--- js/interaction.js
+++ js/interaction.js
@@ -1,13 +1,16 @@
 const $openClosePokedex = document.getElementById('open-poke');
 const $caseLefTo = document.getElementById('caseLeft');
 const $openDoor = document.getElementById('door');
+const $lowAdd = document.getElementById('lAdd');
 
 $openClosePokedex.addEventListener('click', function(){
   if ($caseLefTo.classList.contains('leftTo')) {
     $caseLefTo.classList.remove('leftTo');
     $openDoor.classList.remove('open-door-visible');
+    $lowAdd.classList.remove('disable');
   } else {
     $caseLefTo.classList.add('leftTo');
     $openDoor.classList.add('open-door-visible');
+    $lowAdd.classList.add('disable');
   }
 });
