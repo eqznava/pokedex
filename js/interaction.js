260c48108f7f533292d4acea1de209fcad67e11b
Sergio Enriquez <eqz.nava@gmail.com>
2019-08-04 23:26:26 -0500

    upgrade open-poke

--- js/interaction.js
+++ js/interaction.js
@@ -2,15 +2,25 @@ const $openClosePokedex = document.getElementById('open-poke');
 const $caseLefTo = document.getElementById('caseLeft');
 const $openDoor = document.getElementById('door');
 const $lowAdd = document.getElementById('lAdd');
+const $diago = document.getElementById('diag');
+const $dOne = document.getElementById('do');
+const $dTwo = document.getElementById('dt');
+
 
 $openClosePokedex.addEventListener('click', function(){
   if ($caseLefTo.classList.contains('leftTo')) {
     $caseLefTo.classList.remove('leftTo');
     $openDoor.classList.remove('open-door-visible');
     $lowAdd.classList.remove('disable');
+    $diago.classList.remove('backDarck');
+    $dOne.classList.remove('backDarck');
+    $dTwo.classList.remove('backDarck');
   } else {
     $caseLefTo.classList.add('leftTo');
     $openDoor.classList.add('open-door-visible');
     $lowAdd.classList.add('disable');
+    $diago.classList.add('backDarck');
+    $dOne.classList.add('backDarck');
+    $dTwo.classList.add('backDarck');
   }
 });
